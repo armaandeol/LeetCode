b6b70cd8e5055d5096e69f3b9dc7ea28e72f4615
Armaan <armaan.deol@proton.me>
2024-09-26 23:02:03 +0530

    Time: 455 ms (74.56%), Space: 28 MB (77.28%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -5,11 +5,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] in window:
                 return True
-            
-            # Add current element to the set
             window.add(nums[i])
-            
-            # Maintain window size of at most k
             if len(window) > k:
                 window.remove(nums[i - k])
         
