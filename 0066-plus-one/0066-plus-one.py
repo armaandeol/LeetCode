426df99a1bba175df6a42fabe34312c98317f32c
Armaan <armaan.deol@proton.me>
2024-11-08 20:46:49 +0530

    Time: 0 ms (100%), Space: 16.8 MB (18.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
-        summ = int(''.join(map(str, digits)))
+        num = int(''.join(map(str, digits)))
     
-        summ += 1
+        num += 1
         
-        return [int(digit) for digit in str(summ)]
+        return [int(digit) for digit in str(num)]
         
         
         
