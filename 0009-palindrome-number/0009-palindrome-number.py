022e9b9927a142335ba25ea4bc534833b20dba91
Armaan <armaan.deol@proton.me>
2024-12-02 23:08:54 +0530

    Time: 8 ms (52.04%), Space: 17.4 MB (5.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        return x == x[::-1]
+        revNum = 0
+        dup = x
+        while x > 0:
+            digit = x%10
+            revNum *= 10
+            revNum += digit
+
+            x = x // 10
+
+        return dup == revNum
         
