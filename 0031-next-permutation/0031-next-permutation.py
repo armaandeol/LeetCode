acdd6500e81e3c0116e474a6d5d3b8ee1a5798fc
Armaan <armaan.deol@proton.me>
2024-12-19 06:07:18 +0530

    Time: 0 ms (100%), Space: 17.7 MB (5.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,22 +1,21 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        right = len(nums)-1
-        while right > 0 and nums[right-1] >= nums[right]:
+        right = len(nums) - 1
+        while right > 0 and nums[right - 1] >= nums[right]:
             right -= 1
+
         if right == 0:
-            return self.reverse(nums,0,len(nums)-1)
-        pivot = right-1
-        successor = 0
-        for i in range(len(nums)-1,pivot,-1):
-            if nums[i] > nums[pivot]:
-                successor = i
-                break
-        nums[pivot],nums[successor] = nums[successor],nums[pivot]  
-        self.reverse(nums,pivot+1,len(nums)-1)
-    
-    def reverse(self,nums,l,r):
-        while l < r:
-            nums[l],nums[r] = nums[r],nums[l]
-            l += 1
-            r -= 1
+            nums.reverse()
+            return nums
+
+        pivot = right - 1
+        successor = len(nums) - 1
+        while nums[successor] <= nums[pivot]:
+            successor -= 1
+
+
+        nums[pivot], nums[successor] = nums[successor], nums[pivot]
+
+
+        nums[pivot + 1:] = reversed(nums[pivot + 1:])
         
