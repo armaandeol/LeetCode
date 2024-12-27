be421c43bbd0bb1b33261661726ff077b811570a
Armaan <armaan.deol@proton.me>
2024-12-27 22:46:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (5.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums) // 3
+        i = len(nums) // 3
         ans = []
 
         counter = Counter(nums)
 
         for num, count in counter.items():
-            if count > n:
+            if count > i:
                 ans.append(num)
 
         return ans
