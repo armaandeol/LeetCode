267941f8e5e3ee3494d7a190a3d54b494cabb837
Armaan <armaan.deol@proton.me>
2025-03-06 20:29:48 +0530

    Time: 0 ms (100%), Space: 19.6 MB (90.51%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,12 +3,12 @@ class Solution:
         intervals.append(newInterval)
         intervals.sort()
 
-        ques = [intervals[0]]
+        res = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            if ques[-1][1] >= intervals[i][0]:
-                ques[-1][1] = max(ques[-1][1], intervals[i][1])
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
-                ques.append(intervals[i])
+                res.append(intervals[i])
 
-        return ques
+        return res
