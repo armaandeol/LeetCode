e953e357d14d4c2cf714e3eb43d2e191b4170345
Armaan <armaan.deol@proton.me>
2025-04-08 21:48:19 +0530

    Time: 67 ms (73.03%), Space: 21.5 MB (23.34%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,35 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def construct_dict(word_list):
+            d = {}
+            for word in word_list:
+                for i in range(len(word)):
+                    s = word[:i] + "_" + word[i+1:]
+                    d[s] = d.get(s, []) + [word]
+            return d
+
+        def bfs_words(begin, end, dict_words):
+            queue, visited = deque([(begin, 1)]), set()
+            while queue:
+                word, steps = queue.popleft()
+                if word not in visited:
+                    visited.add(word)
+                    if word == end:
+                        return steps
+                    for i in range(len(word)):
+                        s = word[:i] + "_" + word[i+1:]
+                        neigh_words = dict_words.get(s, [])
+                        for neigh in neigh_words:
+                            if neigh not in visited:
+                                queue.append((neigh, steps + 1))
+            return 0
+
+        if endWord not in wordList:
+            return 0
+
+        
+        word_set = set(wordList)
+        word_set.add(beginWord)
+        d = construct_dict(word_set)
+
+        return bfs_words(beginWord, endWord, d)
