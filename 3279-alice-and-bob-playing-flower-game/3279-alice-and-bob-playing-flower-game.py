89c028493f3fc8e9a6c69361d37e4c4f20cd73b0
Armaan <armaan.deol@proton.me>
2025-08-29 18:33:07 +0530

    Time: 0 ms (100%), Space: 18 MB (15.97%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        # count = 0
+        # a,b = 0,0 
+        # while a != n:
+        #     while b != n:
+
+                
+        return (m*n)//2
